0cd90c23d55896f1c15352a71a4f604737c48cb2
Tim Pizey <pizeyt@elsevier.com>
2016-04-19 11:57:31 +0100

    Add screenshot taking for failed tests

--- src/test/java/net/paneris/juice/LoginIT.java
+++ src/test/java/net/paneris/juice/LoginIT.java
@@ -1,45 +1,43 @@
 package net.paneris.juice;
 
 import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.regex.Pattern;
-
+import static org.junit.Assert.fail;
 
 /**
  * @author timp
  * @since 2016-04-17
  */
 public class LoginIT {
-  protected WebDriver driver;
-  protected WebDriverBackedSelenium selenium;
-  protected String baseUrl = "http://127.0.0.1:8080";
 
+  protected static WebDriver driver;
+  protected static WebDriverBackedSelenium selenium;
+  protected static String baseUrl = "http://127.0.0.1:8080";
 
-  @Before
-  public void setup() {
+  @Rule
+  public ScreenshotFailedTestRule screenshotTestRule = new ScreenshotFailedTestRule(driver);
+
+  @BeforeClass
+  public static void setup() {
     driver = new FirefoxDriver();
     selenium = new WebDriverBackedSelenium(driver, getBaseUrl());
   }
 
-  public String getBaseUrl() {
+  public static String getBaseUrl() {
     String setUrl = System.getenv("JUICE_URL");
     if (setUrl != null) {
       baseUrl = setUrl;
     }
+    System.out.println("Testing " + baseUrl);
     return baseUrl;
   }
 
-  @After
-  public void destroy() {
+  @AfterClass
+  public static void destroy() {
     driver.quit();
   }
 
@@ -50,18 +48,14 @@ public class LoginIT {
     submit("unknown", "password", "Try again");
     submit("unknown", "bad", "Try again");
   }
+
   private void submit(String username, String password, String result) {
     selenium.open("/");
-    driver.manage().window().maximize();
-    WebElement loginText = driver.findElement(By.id("username"));
-    loginText.sendKeys(username);
-    WebElement passwordText = driver.findElement(By.id("password"));
-    passwordText.sendKeys(password);
+    driver.findElement(By.id("username")).sendKeys(username);
+    driver.findElement(By.id("password")).sendKeys(password);
     driver.findElement(By.id("submitLogin")).click();
-    WebDriverWait wait = new WebDriverWait(driver, 3);
-    wait.until(ExpectedConditions
-            .textMatches(
-                    By.ByXPath.xpath("//body"), Pattern.compile(result)));
-
+    if(!selenium.isTextPresent(result)) {
+      fail("Text not found:'" + result + "', see screenshot.");
+    }
   }
 }
---
+++ src/test/java/net/paneris/juice/ScreenshotFailedTestRule.java
@@ -0,0 +1,54 @@
+package net.paneris.juice;
+
+import org.junit.rules.MethodRule;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * See http://blogs.steeplesoft.com/posts/2012/grabbing-screenshots-of-failed-selenium-tests.html
+ *
+ * @author pizeyt
+ * @since 2016-04-19
+ */
+public class ScreenshotFailedTestRule implements MethodRule {
+
+  WebDriver driver;
+
+  public ScreenshotFailedTestRule(WebDriver driver) {
+    this.driver = driver;
+  }
+
+  public Statement apply(final Statement statement,
+                         final FrameworkMethod frameworkMethod, final Object o) {
+    return new Statement() {
+      @Override
+      public void evaluate() throws Throwable {
+        try {
+          statement.evaluate();
+        } catch (Throwable t) {
+          captureScreenshot(frameworkMethod.getName());
+          throw t; // rethrow to allow the failure to be reported to JUnit
+        }
+      }
+
+      public void captureScreenshot(String fileName) {
+        try {
+          new File("target/surefire-reports/").mkdirs();
+          FileOutputStream out = new FileOutputStream("target/surefire-reports/screenshot-"
+              + fileName + ".png");
+          out.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
+          out.close();
+        } catch (Exception e) {
+          // No need to crash the tests if the screenshot fails
+          e.printStackTrace();
+        }
+      }
+    };
+  }
+}
