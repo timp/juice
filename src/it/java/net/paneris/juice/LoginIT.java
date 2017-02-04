4dfaa8448ba6050504b97cca8832242764618629
timp21337 <timp21337@paneris.org>
2017-02-04 14:37:48 +0000

    Upgrade jacoco, use Chrome driver

--- src/it/java/net/paneris/juice/LoginIT.java
+++ src/it/java/net/paneris/juice/LoginIT.java
@@ -1,9 +1,13 @@
 package net.paneris.juice;
 
 import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.Assert.fail;
@@ -23,7 +27,8 @@ public class LoginIT {
 
   @BeforeClass
   public static void setup() {
-    driver = new FirefoxDriver();
+    //driver = new FirefoxDriver();
+    driver = new ChromeDriver();
     selenium = new WebDriverBackedSelenium(driver, getBaseUrl());
   }
 
