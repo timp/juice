3dcf7539849e92ac49b09695b68eefa404431516
Tim Pizey <pizeyt@elsevier.com>
2017-02-06 11:00:11 +0000

    Try firefox

--- src/it/java/net/paneris/juice/LoginIT.java
+++ src/it/java/net/paneris/juice/LoginIT.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.Assert.fail;
 
@@ -24,8 +25,8 @@ public class LoginIT {
 
   @BeforeClass
   public static void setup() {
-    //driver = new FirefoxDriver();
-    driver = new ChromeDriver();
+    driver = new FirefoxDriver();
+    //driver = new ChromeDriver();
   }
 
 
