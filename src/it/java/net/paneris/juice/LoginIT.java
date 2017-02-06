2df3de59b670c3acf461e679f5057076a15de623
Tim Pizey <pizeyt@elsevier.com>
2017-02-06 15:00:42 +0000

    Onwards

--- src/it/java/net/paneris/juice/LoginIT.java
+++ src/it/java/net/paneris/juice/LoginIT.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.Assert.fail;
 
